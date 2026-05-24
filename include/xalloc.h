ce9809434daf51b9b6a89ad338fa976637bbdaa4
Jiamu Sun <39@barroit.sh>
2026-05-24 16:53:07 +0900

    xalloc: add wrappers for calloc, realloc and strdup
    Extend the xalloc API with xcalloc, xrealloc, and xstrdup while
    preserving call-site diagnostics through the shared __xalloc macro.
    Keep large-allocation warnings consistent across the allocation helpers.
    
    Signed-off-by: Jiamu Sun <39@barroit.sh>

--- include/xalloc.h
+++ include/xalloc.h
@@ -10,14 +10,11 @@
 #include "calc.h"
 #include "compiler.h"
 
-#define xmalloc(size) __xmalloc(__FILE__, __LINE__, __func__, size)
-#define xreallocarray(ptr, nmemb, size) \
-	__xreallocarray(__FILE__, __LINE__, __func__, ptr, nmemb, size)
-
-void *__xmalloc(const char *file, int line, const char *func, size_t size);
-
-void *__xreallocarray(const char *file, int line, const char *func,
-		      void *ptr, size_t nmemb, size_t size);
+#define xcalloc(...)       __xalloc(calloc, __VA_ARGS__)
+#define xmalloc(...)       __xalloc(malloc, __VA_ARGS__)
+#define xrealloc(...)      __xalloc(realloc, __VA_ARGS__)
+#define xreallocarray(...) __xalloc(reallocarray, __VA_ARGS__)
+#define xstrdup(...)       __xalloc(strdup, __VA_ARGS__)
 
 #define XREALLOCBUF(x, new, cap)			\
 do {							\
@@ -28,5 +25,21 @@ do {							\
 		cap = next_buf_size(new);		\
 	x = xreallocarray(x, sizeof(*(x)), cap);	\
 } while (0)
+
+#define __xalloc(name, ...) \
+	__x ## name(__FILE__, __LINE__, __func__, __VA_ARGS__)
+
+void *__xcalloc(const char *file, int line, const char *func, size_t nmemb,
+		size_t size);
+
+void *__xmalloc(const char *file, int line, const char *func, size_t size);
+
+void *__xrealloc(const char *file, int line, const char *func, void *ptr,
+		 size_t size);
+
+void *__xreallocarray(const char *file, int line, const char *func, void *ptr,
+		      size_t nmemb, size_t size);
+
+char *__xstrdup(const char *file, int line, const char *func, const char *s);
 
 #endif /* XALLOC_H */
--- lib/xalloc.c
+++ lib/xalloc.c
@@ -5,6 +5,7 @@
 
 #include <inttypes.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "calc.h"
@@ -26,8 +27,8 @@ static inline void assert_allocated(const char *file, int line,
 }
 
 #ifdef CONFIG_WARN_LARGE_ALLOC
-static inline void warn_on_large_alloc(const char *file, int line,
-				       const char *func, uintmax_t n)
+static inline void warn_large_alloc(const char *file, int line,
+				    const char *func, uintmax_t n)
 {
 	if (n < CONFIG_LARGE_ALLOC)
 		return;
@@ -37,25 +38,59 @@ static inline void warn_on_large_alloc(const char *file, int line,
 		file, line, func, n, (uintmax_t)CONFIG_LARGE_ALLOC);
 }
 #else
-# define warn_on_large_alloc NOOP
+# define warn_large_alloc NOOP
 #endif
+
+void *__xcalloc(const char *file, int line, const char *func, size_t nmemb,
+		size_t size)
+{
+	size_t nalloc = mult_or_die(nmemb, size);
+	void *buf = calloc(nmemb, size);
+
+	assert_allocated(file, line, func, buf, nalloc);
+	warn_large_alloc(file, line, func, nalloc);
+	return buf;
+}
 
 void *__xmalloc(const char *file, int line, const char *func, size_t size)
 {
 	void *buf = malloc(size);
 
 	assert_allocated(file, line, func, buf, size);
-	warn_on_large_alloc(file, line, func, size);
+	warn_large_alloc(file, line, func, size);
 	return buf;
 }
 
-void *__xreallocarray(const char *file, int line, const char *func,
-		      void *ptr, size_t nmemb, size_t size)
+void *__xrealloc(const char *file, int line, const char *func, void *ptr,
+		 size_t size)
 {
-	uintmax_t total = mult_or_die(nmemb, size);
+	void *buf = realloc(ptr, size);
+
+	assert_allocated(file, line, func, buf, size);
+	warn_large_alloc(file, line, func, size);
+	return buf;
+}
+
+void *__xreallocarray(const char *file, int line, const char *func, void *ptr,
+		      size_t nmemb, size_t size)
+{
+	uintmax_t nalloc = mult_or_die(nmemb, size);
 	void *buf = reallocarray(ptr, nmemb, size);
 
-	assert_allocated(file, line, func, buf, total);
-	warn_on_large_alloc(file, line, func, total);
+	assert_allocated(file, line, func, buf, nalloc);
+	warn_large_alloc(file, line, func, nalloc);
 	return buf;
+}
+
+char *__xstrdup(const char *file, int line, const char *func, const char *s)
+{
+	char *buf = strdup(s);
+
+	if (buf)
+		return buf;
+
+	logf_fd(STDERR_FILENO, NULL, NULL,
+		"%s:%d,%s(): failed to duplicate string '%.10s...'",
+		file, line, func, s);
+	abort();
 }
